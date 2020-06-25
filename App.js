1835dcd955400d7fcb3a336210c78bb13fe8ee01
groot8 <nader@getspokn.com>
2020-06-25 22:14:12 +0200

    :wrench: configure eslint & prettier

--- App.js
+++ App.js
@@ -6,11 +6,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
